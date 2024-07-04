f67fcf3b7ae250e4c1776d44f96a316c4a0e9dfc
Callum Thomson <callum@callumthomson.co.uk>
2024-07-04 18:05:49 +0100

    Remove hrtime option

--- src/sort-id.ts
+++ src/sort-id.ts
@@ -3,18 +3,12 @@ import { randomString } from './random-string';
 export type SortIdParams = {
   prefix?: string;
   separator?: string;
-  time?: 'hrtime' | 'date';
   alphabet?: string;
   randStrLength?: number;
   randStrProvider?: () => string;
 };
 
 export const sortId = (params?: SortIdParams): string => {
-  const ts =
-    params?.time === 'hrtime'
-      ? process.hrtime.bigint().toString(36)
-      : Date.now().toString(36);
-
   const randStr =
     typeof params?.randStrProvider !== 'undefined'
       ? params.randStrProvider()
@@ -23,7 +17,7 @@ export const sortId = (params?: SortIdParams): string => {
           length: params?.randStrLength || 5,
         });
 
-  let id = `${ts}${randStr}`;
+  let id = `${Date.now().toString(36)}${randStr}`;
   if (params?.prefix) {
     id = `${params.prefix}${params.separator || '_'}${id}`;
   }
